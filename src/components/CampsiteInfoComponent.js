c865f72e5f32d24352aab402848e6b3459437bd0
Krizthel Legaspi <kablegaspi@gmail.com>
2020-03-23 21:03:51 -0400

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
+
    
-    renderCampsite(campsite) {
+function RenderCampsite({campsite}) {
         return(
             <div className="col-md-5 m-1">
                 <Card>
@@ -17,13 +17,13 @@ class CampsiteInfo extends Component {
         );
 
     }
-    renderComments(campsite){
-        if(this.props.campsite.comments){
+function RenderComments({comments}){
+        if(comments){
             return (
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
                    { 
-                        this.props.campsite.comments.map(comment => <div key={comment.id}><p>{comment.text} <br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)
+                        comments.map(comment => <div key={comment.id}><p>{comment.text} <br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)
                         
                     }  
                 </div>
@@ -32,13 +32,13 @@ class CampsiteInfo extends Component {
         return <div />;
     }
     
-    render() {
-        if (this.props.campsite) {
+function CampsiteInfo(props) {
+        if (props.campsite) {
             return (
             <div className="container">
                 <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
                 </div>
             </div>
         );
@@ -47,7 +47,7 @@ class CampsiteInfo extends Component {
 }
 
    
-}
+
 
 
 export default CampsiteInfo;
