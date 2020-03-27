9e755e0369cf70ccb09cf37a26dae35fd7df43e1
Krizthel Legaspi <kablegaspi@gmail.com>
2020-03-26 23:29:53 -0400

    breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
    
@@ -9,9 +10,8 @@ function RenderCampsite({campsite}) {
                 <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
                 </Card>
             </div>
         );
@@ -35,13 +35,23 @@ function RenderComments({comments}){
 function CampsiteInfo(props) {
         if (props.campsite) {
             return (
-            <div className="container">
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.comments} />
+                    </div>
                 </div>
-            </div>
-        );
+            );
     }
     return <div />;
 }
