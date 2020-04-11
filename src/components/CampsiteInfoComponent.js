adb1948c0ac052dc68c8a51b3ad9b3a81ec8361d
Krizthel Legaspi <kablegaspi@gmail.com>
2020-04-11 14:11:46 -0400

    fetch post comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-//import React from 'react';
 import { Card, CardImg, CardText, CardBody,  Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render()
@@ -118,7 +118,7 @@ function RenderCampsite({campsite}) {
         return(
             <div className="col-md-5 m-1">
                 <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                     <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
@@ -127,7 +127,7 @@ function RenderCampsite({campsite}) {
         );
 
     }
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
         if(comments){
             return (
                 <div className="col-md-5 m-1">
@@ -136,7 +136,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                        comments.map(comment => <div key={comment.id}><p>{comment.text} <br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)
                        
                     }  
-                   { <CommentForm campsiteId={campsiteId} addComment={addComment} />}
+                   {   <CommentForm campsiteId={campsiteId} postComment={postComment} />}
                 </div>
             );
         }
@@ -182,7 +182,7 @@ function CampsiteInfo(props) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                     </div>
